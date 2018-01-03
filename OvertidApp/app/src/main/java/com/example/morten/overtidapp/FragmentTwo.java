10a141462d56fce0b0905715ac1dca635adb4eed
Unknown <mortenolsen4@gmail.com>
2018-01-03 20:40:20 +0100

    Lagde xml for en spinner

--- OvertidApp/app/src/main/java/com/example/morten/overtidapp/FragmentTwo.java
+++ OvertidApp/app/src/main/java/com/example/morten/overtidapp/FragmentTwo.java
@@ -7,7 +7,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -15,12 +18,14 @@ import java.util.ArrayList;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class FragmentTwo extends android.support.v4.app.Fragment implements AdapterView.OnItemClickListener{
+public class FragmentTwo extends android.support.v4.app.Fragment implements AdapterView.OnItemClickListener {
     static ArrayList<Overtid> overtidListe;
 
     private ListView startListView;
-private MyDbHandler db;
+    private MyDbHandler db;
     OvertidsAdapter adapter;
+    Spinner spinner;
+
     public FragmentTwo() {
         // Required empty public constructor
     }
@@ -34,11 +39,18 @@ private MyDbHandler db;
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_fragment_two2, container, false);
 
-        db= new MyDbHandler(getActivity(),null,null,MyDbHandler.DATABASEVERSJON);
+        db = new MyDbHandler(getActivity(), null, null, MyDbHandler.DATABASEVERSJON);
         startListView = (ListView) view.findViewById(R.id.startList);
-       startListView.setOnItemClickListener(this);
+        startListView.setOnItemClickListener(this);
         adapter = new OvertidsAdapter(getContext(), MainActivity.overtid);
         startListView.setAdapter(adapter);
+
+        spinner = (Spinner) view.findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.mnd_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
 
 
         return view;
@@ -55,27 +67,38 @@ private MyDbHandler db;
             MainActivity.overtid.remove(position);
             adapter.notifyDataSetChanged();
 
-           //Oppdaterer fragOne tekstene
+            //Oppdaterer fragOne tekstene
             FragmentOne.antTimer.setText(Overtid.visTotatl());
             FragmentOne.visTotalsum.setText(Double.toString(Overtid.visTotatlIntjent()));
             FragmentOne.timerDenneMnd.setText(Double.toString(Overtid.timerDenneMnd()));
             FragmentOne.progressStatus = Overtid.avstandTilTargetSum();
             FragmentOne.progressBar.setProgress(FragmentOne.progressStatus);
-            FragmentOne. timerCurrMnd=Double.toString(Overtid.timerDenneMnd());
-            FragmentOne.  sumDenneMnd.setText(FragmentOne.sumCurrMnd);
-            FragmentOne.progressStatusDenneMND=Overtid.avstandDenneMND();
-            FragmentOne.  progressDenneMND.setProgress(FragmentOne.progressStatusDenneMND);
+            FragmentOne.timerCurrMnd = Double.toString(Overtid.timerDenneMnd());
+            FragmentOne.sumDenneMnd.setText(FragmentOne.sumCurrMnd);
+            FragmentOne.progressStatusDenneMND = Overtid.avstandDenneMND();
+            FragmentOne.progressDenneMND.setProgress(FragmentOne.progressStatusDenneMND);
 
 
         }
 
 
+    }
 
-
+    public void onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
 
     }
 
-    public void onItemLongClick(AdapterView<?> parent, View view, int position, long id){
+//Spinner onClick metoder
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
 
+        // An item was selected. You can retrieve the selected item using
+        //int res= (int) parent.getItemAtPosition(pos);
+        Toast.makeText(getActivity(), "Hei fra spinner: " + pos, Toast.LENGTH_SHORT).show();
     }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
+
+
 }
